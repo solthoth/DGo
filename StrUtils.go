38b2762678f98c9698c1dbdf96e347ed5ccfd748
Carlos Barajas <Carlos.Barajas@sbtpg.com>
2015-03-04 16:57:27 -0800

    Add function string to int32

--- StrUtils.go
+++ StrUtils.go
@@ -5,3 +5,9 @@ import "strconv"
 func IntToStr(value int) string {
   return strconv.FormatInt(int64(value),10)
 }
+
+func StrToInt(value string) int32 {
+  iVal, err := strconv.ParseInt(value,10,32)
+  if (err != nil){}
+  return int32(iVal)
+}
